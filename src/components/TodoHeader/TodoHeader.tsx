4573e768ca55917b0e5956351bb7400afa7a9ff8
王立广 <wangliguang@huoban.com>
2022-08-24 22:05:21 +0800

    feat: 基本布局

---
+++ src/components/TodoHeader/TodoHeader.tsx
@@ -0,0 +1,10 @@
+export function TodoHeader() {
+  
+    return (
+        <div className="header">
+            <span className='logo'>猿TODO</span>
+                
+        
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
 )
